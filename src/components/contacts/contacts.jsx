470f10cd24fc90ce379e328141b16596c20b7c6d
angelkapv81@gmail.com <angelkapv81>
2023-05-07 20:01:02 +0300

    fix props

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -23,5 +23,15 @@ const Contacts = ({ contacts, onDelete }) => {
     </div>
   );
 };
+Search.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default Contacts;
